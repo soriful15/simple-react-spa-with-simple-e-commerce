627ed9b149313dc1e62f5b4222df031f1bf2d68c
soriful15 <sorifulislam.rs10@gmailcomm>
2023-03-27 13:15:59 +0600

    navbar Complete

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Components/Header/Header';
+
+const App = () => {
+  return (
+    <div className='mx-auto container'>
+     <Header></Header>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from '../../images/Logo.svg'
+const Header = () => {
+    return (
+        <>
+            <div className='h-20 bg-gray-700 flex justify-between px-24 py-4 mt-4 '>
+                <div>
+                    <img src={logo} alt="" />
+                </div>
+                <div>
+                    <nav className='text-slate-300 text-lg font-bold '>
+                       <a className='ml-4 hover:text-orange-800' href="/Order">Order</a> 
+                       <a className='ml-4 hover:text-orange-800' href="/Order Review">Order Review</a> 
+                       <a className='ml-4 hover:text-orange-800' href="/Manage Inventory">Manage Inventory</a> 
+                       <a className='ml-4 hover:text-orange-800' href="/Login">Login</a> 
+                    </nav>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Header;
