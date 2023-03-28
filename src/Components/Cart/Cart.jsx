373a8538149522cd4c81e2eee3e4b7b3bf1807e3
soriful15 <sorifulislam.rs10@gmailcomm>
2023-03-28 15:53:59 +0600

    save to cart  local storage

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -14,9 +14,9 @@ totalShipping=totalShipping+product.shipping
 
 
     }
-    const tax=((totalPrice*7)/100).toFixed(2)
+    const tax=((totalPrice*7)/100)
     const grandTotaL=parseFloat(totalPrice)+parseFloat(totalShipping)+parseFloat(tax)
-    const allGrandTotal=grandTotaL.toFixed(2)
+   
     return (
         <>
             <div className='mt-6 text-lg font-medium sticky top-0'>
@@ -24,8 +24,8 @@ totalShipping=totalShipping+product.shipping
                     <p className=' mt-6'>Selected Item: {cart.length}</p>
                     <p className='mt-4'>Total Price: ${totalPrice}</p>
                     <p className='mt-4'>Total Shipping Charge: ${totalShipping}</p>
-                    <p className='mt-4'>Tax: ${tax}</p>
-                    <h6 className='text-xl font-bold mt-4'>Grand Total: ${allGrandTotal}</h6>
+                    <p className='mt-4'>Tax: ${tax.toFixed(2)}</p>
+                    <h6 className='text-xl font-bold mt-4'>Grand Total: ${grandTotaL.toFixed(2)}</h6>
             
             </div>
         </>
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -10,13 +11,19 @@ const Shop = () => {
         .then(res=>res.json())
         .then(data=>setProducts(data))
     },[])
+    useEffect(()=>{
+        const shoppingCart=getShoppingCart()
+        console.log(shoppingCart);
+    },[])
 
     const handleAddToCart=(product)=>{
         // console.log(product)
         // cart.push(product)
         const newCart=[...cart,product]
         setCart(newCart)
+        addToDb(product.id)
     }
+  
     return (
         <>
             <div className='shop-container'>
